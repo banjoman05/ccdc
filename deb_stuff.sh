3ed7e7ecfa356c6758a07cbd35aed8df64a710c3
banjoman05 <kory.miller@gmail.com>
2017-02-09 18:50:49 -0500

    Update deb_stuff.sh

--- deb_stuff.sh
+++ deb_stuff.sh
@@ -4,3 +4,7 @@
 
 echo "deb http://archive.debian.org/debian-archive/debian/ lenny main contrib non-free" >> /etc/apt/sources.list
 echo "deb http://archive.debian.org/debian-security/ lenny/updates main contrib non-free" >> /etc/apt/sources.list
+
+apt-get update
+apt-get install -y vim htop sudo fail2ban ufw unzip screen
+
