a36e46013c8990d933b3cb2b738de96146a5872c
banjoman05 <kory.miller@gmail.com>
2017-02-09 20:53:50 -0500

    Update deb_stuff.sh

--- deb_stuff.sh
+++ deb_stuff.sh
@@ -20,6 +20,31 @@ iptables -A INPUT -p tcp --dport 443 -j ACCEPT
 iptables -P FORWARD DROP
 iptables -P OUTPUT ACCEPT
 
+chsh -s /bin/false daemon
+chsh -s /bin/false bin
+chsh -s /bin/false sys
+chsh -s /bin/false games
+chsh -s /bin/false man
+chsh -s /bin/false mail
+chsh -s /bin/false news
+chsh -s /bin/false uucp
+chsh -s /bin/false proxy
+chsh -s /bin/false www-data
+chsh -s /bin/false backup
+chsh -s /bin/false list
+chsh -s /bin/false irc
+chsh -s /bin/false gnats
+chsh -s /bin/false nobody
+chsh -s /bin/false libuuid
+chsh -s /bin/false mailserver
+
+
+
+
+
+
+
+
 
 
 
